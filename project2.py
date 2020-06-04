a4be750af0eba2d2c8ad38c8e3d019416b82c740
Sweta Gadhiya <swetagadhiya123@gmail.com>
2020-06-04 14:47:27 -0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -21,7 +21,7 @@ def loadDataset(filename):
     return dataset
 
 
-# In the subpartone i.e, project 2 task A,
+# In the subpartone i.e, project 2 task A,Conflict
 # we have to write a sub routine which computes the distance between two data instances based on their category (numerical or categorical so called string type data.)
 def distance(training_instance, test_instance):
     # findDistanceBetweenTwoInstances
